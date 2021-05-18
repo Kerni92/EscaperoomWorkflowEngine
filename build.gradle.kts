56c8b28e03cc93282828680c93eb9c8d9e5f6e28
Daniel Kernebeck <d.kernebeck@gmx.net>
2021-05-18 22:16:08 +0200

    Enable spring security with basicauth for import api

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ version = "0.0.1-SNAPSHOT"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-//    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.mariadb.jdbc:mariadb-java-client")
     implementation("org.flywaydb:flyway-core")
 
