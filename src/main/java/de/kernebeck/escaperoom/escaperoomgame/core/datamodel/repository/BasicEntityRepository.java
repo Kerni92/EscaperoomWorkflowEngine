6df2b5636d78eb8f943b52d23785439550965e08
Daniel Kernebeck <d.kernebeck@gmx.net>
2021-08-25 22:20:32 +0200

    Change BasicEntityRepository to extend from JPARepository

--- src/main/java/de/kernebeck/escaperoom/escaperoomgame/core/datamodel/repository/BasicEntityRepository.java
+++ src/main/java/de/kernebeck/escaperoom/escaperoomgame/core/datamodel/repository/BasicEntityRepository.java
@@ -1,10 +1,10 @@
 package de.kernebeck.escaperoom.escaperoomgame.core.datamodel.repository;
 
 import de.kernebeck.escaperoom.escaperoomgame.core.datamodel.entity.BasicEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
-public interface BasicEntityRepository<T extends BasicEntity> extends CrudRepository<T, Long> {
+public interface BasicEntityRepository<T extends BasicEntity> extends JpaRepository<T, Long> {
 
 }
